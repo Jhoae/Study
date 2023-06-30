56ce9d76c036b612cd4aed89690f7c28509f1318
Jho2e <ohjaehokor@naver.com>
2023-06-30 15:57:00 +0900

    C++ Section1_6 Identifier

---
+++ 홍정모의 따라하며 배우는 C++/Section 1- Basic usage of C++/1_6 keyword, Identifier/1_6.cpp
@@ -0,0 +1,19 @@
+void getNumber() // 필요하다면 주석을 달아두기 ...
+{
+
+}
+
+class MyClass
+{
+public:
+	int m_variable;
+};
+
+int main() 
+{
+	int n_apples;
+	int nApples;
+	int _apples; // row level에서 개발할때 주로 사용했었음.
+
+	getNumber(); // Go To Definition
+}
